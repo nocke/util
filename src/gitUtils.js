0d9e9fc60b89a0da3b1e04ca866244866a4a94a6
_USER_ <_@_>
2023-02-17 16:47:09 +0100

    gitUtils: fix returned type

--- src/gitUtils.js
+++ src/gitUtils.js
@@ -5,10 +5,10 @@ import { guard } from './execute.js'
 export const getGitProp = (key /* COULDDO: , scope=<default> */) => {
   const r = guard(`git config --get ${key}`, { mute: true })
   if (r === 'false' || r === 'true') {
-    return r === 'true'
+    return r === 'true' // return as boolean
   }
-  if (Number.isNaN(Number(r))) {
-    return r
+  if (!Number.isNaN(Number(r))) {
+    return Number(r) // return as number
   }
   return r
 }
